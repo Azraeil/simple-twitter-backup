20e482349faa62ad499ca625f5619cc38f923bac
Shih_Kuan <cliff1304@gmail.com>
2018-03-26 20:58:37 +0800

    Add index and show actioins for tweet api.

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,2 +1,34 @@
-class Api::V1::TweetsController < ApplicationController
+class Api::V1::TweetsController < ApiController
+  def index
+    @tweets = Tweet.all
+    render json: {
+      # 用 data: 排版 JSON 輸出格式
+      # 用 map 重新映射一個新的陣列，只留 API 想給 client 的資料
+      data: @tweets.map do |tweet|
+        {
+          id: tweet.id,
+          date: tweet.created_at.strftime("%Y-%m-%d"),
+          description: tweet.description
+        }
+      end
+    }
+  end
+
+
+  def show
+    # 用 find_by 在找不到 tweet 時，回傳 nil
+    @tweet = Tweet.find_by(id: params[:id])
+    if !@tweet
+      render json: {
+        message: "Can't find the tweet!",
+        status: 400
+      }
+    else
+      render json: {
+        id: @tweet.id,
+        date: @tweet.created_at.strftime("%Y-%m-%d"),
+        description: @tweet.description
+      }
+    end
+  end
 end
