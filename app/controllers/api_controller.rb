bbeae3fd50fb9210b9569238c464fbc51fc6a0f2
Shih_Kuan <cliff1304@gmail.com>
2018-03-26 22:32:32 +0800

    Add authenticate_user_from_token in ApiController.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,14 @@
 class ApiController < ActionController::Base
+  before_action :authenticate_user_from_token!
   before_action :authenticate_user!
+
+  def authenticate_user_from_token!
+    if params[:auth_token].present?
+      user = User.find_by_authentication_token(params[:auth_token])
+
+      # sign_in 是 Devise 提供的
+      sign_in(user, store: false) if user
+    end
+  end
+
 end
